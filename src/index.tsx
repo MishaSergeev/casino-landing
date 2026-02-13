7af57d8fdf2cff0b005358e2c06ffc4a274f858c
Mikhail Sergeev <sergeev.mikhail.serg@gmail.com>
2026-02-13 11:33:54 +0200

    Updates

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client"
-import App from "./app/App"
-import "./shared/i18n"
-
-const root = createRoot(document.getElementById("root")!)
-root.render(<App />)
--- src/theme/index.ts
+++
@@ -1,2 +0,0 @@
-export { themes, ThemeName, AppTheme } from "./theme";
-export { ThemeProviderApp, useThemeApp } from "./ThemeContext";
